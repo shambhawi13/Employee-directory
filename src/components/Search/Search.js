a5f62f8afbc74283d20fd8cf9e1c9fcdda2cdecc
Sham <shambhawi.kumari1393@gmail.com>
2020-08-11 23:26:52 -0700

    added search box UI

---
+++ src/components/Search/Search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Search(){
+    return(
+        <input className="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+    )
+}
+
+export default Search;
--- src/pages/EmployeeContainer/EmployeeContainer.js
+++ src/pages/EmployeeContainer/EmployeeContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import EmployeeTable from '../../components/EmployeeTable/EmployeeTable';
+import Search from '../../components/Search/Search';
+import './style.css';
 
 function EmployeeContainer(){
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-12 table-responsive">
+                <div className="col-md-12 search">
+                    <Search></Search>
+                </div>
+                <div className="col-md-12 table-responsive emp-table">
                     <EmployeeTable></EmployeeTable>
                 </div>
             </div>
