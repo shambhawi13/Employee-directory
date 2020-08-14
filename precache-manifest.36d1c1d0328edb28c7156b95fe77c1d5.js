1b69b84cc2d510cd53510e96f293b17942e249b0
Sham <shambhawi.kumari1393@gmail.com>
2020-08-13 19:11:19 -0700

    Updates

---
+++ precache-manifest.36d1c1d0328edb28c7156b95fe77c1d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a4db01ea050a190c8e979c979aec30",
+    "url": "/Employee-directory/index.html"
+  },
+  {
+    "revision": "591f09011e2bbb9e644b",
+    "url": "/Employee-directory/static/css/main.c5990e60.chunk.css"
+  },
+  {
+    "revision": "de0a78599a4cc7c1df13",
+    "url": "/Employee-directory/static/js/2.53813475.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-directory/static/js/2.53813475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591f09011e2bbb9e644b",
+    "url": "/Employee-directory/static/js/main.886a9ac7.chunk.js"
+  },
+  {
+    "revision": "bafa920a607adc3a8c64",
+    "url": "/Employee-directory/static/js/runtime-main.4e2a4874.js"
+  }
+]);
